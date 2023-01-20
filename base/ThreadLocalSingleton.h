88988ecddd0e0cb56c297c8e073363730f0844a1
bugcat9 <1767508581@qq.com>
2023-01-20 12:26:40 +0800

    add:ThreadLocalSingleton

---
+++ base/ThreadLocalSingleton.h
@@ -0,0 +1,76 @@
+#ifndef _THREADLOCALSINGLETON_H
+#define _THREADLOCALSINGLETON_H
+
+#include <assert.h>
+#include <boost/noncopyable.hpp>
+
+namespace tinyMuduo
+{
+    template <typename T>
+    class ThreadLocalSingleton : boost::noncopyable
+    {
+    public:
+        ThreadLocalSingleton() = delete;
+        ~ThreadLocalSingleton() = delete;
+
+        static T &instance()
+        {
+            if (!t_value_)
+            {
+                t_value_ = new T();
+                deleter_.set(t_value_);
+            }
+            return *t_value_;
+        }
+
+        static T *pointer()
+        {
+            return t_value_;
+        }
+
+    private:
+        static void destructor(void *obj)
+        {
+            assert(obj == t_value_);
+            typedef char T_must_be_complete_type[sizeof(T) == 0 ? -1 : 1];
+            T_must_be_complete_type dummy;
+            (void)dummy;
+            delete t_value_;
+            t_value_ = 0;
+        }
+
+        class Deleter
+        {
+        public:
+            Deleter()
+            {
+                pthread_key_create(&pkey_, &ThreadLocalSingleton::destructor);
+            }
+
+            ~Deleter()
+            {
+                pthread_key_delete(pkey_);
+            }
+
+            void set(T *newObj)
+            {
+                assert(pthread_getspecific(pkey_) == NULL);
+                pthread_setspecific(pkey_, newObj);
+            }
+
+            pthread_key_t pkey_;
+        };
+
+        static __thread T *t_value_;
+        static Deleter deleter_;
+    };
+
+    template <typename T>
+    __thread T *ThreadLocalSingleton<T>::t_value_ = 0;
+
+    template <typename T>
+    typename ThreadLocalSingleton<T>::Deleter ThreadLocalSingleton<T>::deleter_;
+
+} // namespace tinyMuduo
+
+#endif // _THREADLOCALSINGLETON_H
