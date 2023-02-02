133bd30adf5e953e478207d03cfed562fc9eb8c6
bugcat9 <1767508581@qq.com>
2023-02-02 12:46:34 +0800

    add:TcpClient

---
+++ net/Connector.h
@@ -0,0 +1,71 @@
+#ifndef _CONNECTOR_H
+#define _CONNECTOR_H
+
+#include "InetAddress.h"
+
+#include <boost/noncopyable.hpp>
+#include <functional>
+#include <memory>
+
+namespace tinyMuduo
+{
+    namespace net
+    {
+
+        class Channel;
+        class EventLoop;
+
+        class Connector : boost::noncopyable,
+                          public std::enable_shared_from_this<Connector>
+        {
+        public:
+            typedef std::function<void(int sockfd)> NewConnectionCallback;
+
+            Connector(EventLoop *loop, const InetAddress &serverAddr);
+            ~Connector();
+
+            void setNewConnectionCallback(const NewConnectionCallback &cb)
+            {
+                newConnectionCallback_ = cb;
+            }
+
+            void start();   // can be called in any thread
+            void restart(); // must be called in loop thread
+            void stop();    // can be called in any thread
+
+            const InetAddress &serverAddress() const { return serverAddr_; }
+
+        private:
+            enum States
+            {
+                kDisconnected,
+                kConnecting,
+                kConnected
+            };
+            static const int kMaxRetryDelayMs = 30 * 1000;
+            static const int kInitRetryDelayMs = 500;
+
+            void setState(States s) { state_ = s; }
+            void startInLoop();
+            void stopInLoop();
+            void connect();
+            void connecting(int sockfd);
+            void handleWrite();
+            void handleError();
+            void retry(int sockfd);
+            int removeAndResetChannel();
+            void resetChannel();
+
+            EventLoop *loop_;
+            InetAddress serverAddr_;
+            bool connect_; // atomic
+            States state_; // FIXME: use atomic variable
+            std::unique_ptr<Channel> channel_;
+            NewConnectionCallback newConnectionCallback_;
+            int retryDelayMs_;
+        };
+
+    } // namespace net
+} // namespace tinyMuduo
+
+#endif // _CONNECTOR_H
